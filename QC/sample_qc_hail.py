af88e5163577fb06c61291bad75e61da95e5b46d
Besse Cummings <berylc@broadinstitute.org>
2017-08-14 15:15:07 -0400

    Updated sex check filters

--- QC/sample_qc_hail.py
+++ QC/sample_qc_hail.py
@@ -17,41 +17,36 @@ vds = vds.sample_qc()
 vds = vds.variant_qc().cache()
 
 #Filter to biallelic SNPs with AF > 0.01
-vds_sex_check = vds.filter_multi()
-vds_sex_check = vds_sex_check.filter_variants_expr('va.filters.isEmpty() && va.qc.AF > 0.01 && v.altAllele.isSNP()', keep=True)
+#vds_sex_check = vds.filter_multi()
+#vds_sex_check = vds_sex_check.filter_variants_expr('va.filters.isEmpty() && va.qc.AF > 0.01 && v.altAllele.isSNP()', keep=True)
 
 #Impute sex 
-vds_sex_check = vds_sex_check.impute_sex()
+#vds_sex_check = vds_sex_check.impute_sex()
 
 #Also mimic gnomad sex check 
 #X heterozygosity
-vds = vds.annotate_samples_expr('sa.NVarsChrX = gs.filter(g => v.contig == "X").count()')
-vds = vds.annotate_samples_expr('sa.NHetVarsChrX = gs.filter(g => v.contig == "X" && g.isHet()).count()')
+#vds = vds.annotate_samples_expr('sa.NVarsChrX = gs.filter(g => v.contig == "X").count()')
+#vds = vds.annotate_samples_expr('sa.NHetVarsChrX = gs.filter(g => v.contig == "X" && g.isHet()).count()')
 
 #Chrom 20 coverage
-vds = vds.annotate_samples_expr('sa.Cov20 = gs.filter(g => v.contig == "20").map(g => g.dp).stats().sum')
+#vds = vds.annotate_samples_expr('sa.Cov20 = gs.filter(g => v.contig == "20").map(g => g.dp).stats().sum')
 
 #Chrom Y coverage
-vds_y = vds.filter_variants_expr('v.contig == "Y"', keep = True)
-intervals = map(Interval.parse, ['Y:10001-2649520', 'Y:59034050-59363566'])
-vds_y = vds_y.filter_intervals(intervals, keep = False)
-vds_y = vds_y.annotate_samples_expr('sa.CovY = gs.filter(g => v.contig == "Y").map(g => g.dp).stats().sum')
-
-vds_y.count_variants()
+#vds_y = vds.filter_variants_expr('v.contig == "Y"', keep = True)
+#intervals = map(Interval.parse, ['Y:10001-2649520', 'Y:59034050-59363566'])
+#vds_y = vds_y.filter_intervals(intervals, keep = False)
+#vds_y = vds_y.annotate_samples_expr('sa.CovY = gs.filter(g => v.contig == "Y").map(g => g.dp).stats().sum')
 
 #Write file
-vds.export_samples("gs://gnomad-berylc/output/sample_qc_081417.tsv", 'Sample = s, sa.qc.*, sa.imputesex.*,  CovY =  sa.CovY, Cov20 = sa.Cov20, NHetX = sa.NHetVarsChrX, NTotalX = sa.NVarsChrX')
-
-#Export metrics to plot
-#vds.export_samples("gs://gnomad-berylc/output/sample_qc.tsv", 'Sample = s, sa.qc.*, sa.imputesex.*')
+#vds.export_samples("gs://gnomad-berylc/output/sample_qc_081417.tsv", 'Sample = s, sa.qc.*, sa.imputesex.*,  CovY =  sa.CovY, Cov20 = sa.Cov20, NHetX = sa.NHetVarsChrX, NTotalX = sa.NVarsChrX')
 
 #Filter based on QC metrics
-vds = vds.filter_samples_expr('sa.qc.dpMean > 15 && sa.qc.callRate > 0.9 && sa.qc.gqMean > 50 && sa.qc.rTiTv < 2.65 && sa.qc.rHetHomVar >1 && sa.qc.rInsertionDeletion < 1.2')
-#This leaves 14,683 samples
+vds = vds.filter_samples_expr('sa.qc.dpMean > 20 && sa.qc.dpMean < 120 && sa.qc.callRate > 0.95 && sa.qc.gqMean > 60 && sa.qc.rTiTv < 2.6 && sa.qc.rHetHomVar > 1.2 && sa.qc.rInsertionDeletion < 1')
+#This leaves 14,636 samples
 
 #Filter based on sex imputation
-vds = vds.filter_samples_expr('sa.imputesex.Fstat > -0.3', keep = True)
-vds = vds.filter_samples_expr('sa.imputesex.Fstat > 0.35 && sa.imputesex.Fstat < 0.5', keep = False)
+vds = vds.filter_samples_expr('sa.imputesex.Fstat > 0.9 || sa.imputesex.Fstat < 0.3 ', keep = True)
+vds = vds.filter_samples_expr('sa.imputesex.Fstat > -0.25 `', keep = False)
 #First filters 106 samples, second filters 6, can make these stricter in later iterations
 
 #Filter three additional MyoSeq samples where the sex in ped does not match inferred sex
